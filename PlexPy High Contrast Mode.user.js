5257a4628c691c89827462836c41746622a7ea4e
Jonathan Wong <Jonny.Wong.16@gmail.com>
2015-08-23 23:21:37 -0700

    Add episode images on season info pages

--- PlexPy High Contrast Mode.user.js
+++ PlexPy High Contrast Mode.user.js
@@ -15,23 +15,24 @@ waitForKeyElements(".home-platforms-instance-box", imageReplacement);
 waitForKeyElements(".users-poster-face", imageReplacement);
 waitForKeyElements(".user-info-poster-face", imageReplacement);
 waitForKeyElements(".user-platforms-instance-box", imageReplacement);
+waitForKeyElements(".season-episodes-poster-face", imageReplacement);
 
 function imageReplacement (elementToReplace) {
     var img_url, width, height;
     img_url = elementToReplace.attr('style').split('(')[1].split(')')[0];
-    width = img_url.indexOf("width=");
-    width = parseInt(img_url.substring(width+6, width+9).split('&')[0]);
-    height = img_url.indexOf('height=');
-    height = parseInt(img_url.substring(height+7, height+10).split('&')[0]);
-    if (elementToReplace.attr("class").indexOf("home-platforms-instance-box") > -1 || 
+    if (elementToReplace.attr("class").indexOf("users-poster-face") > -1) {
+        width = 40;
+        height = 40;
+    } else if (elementToReplace.attr("class").indexOf("home-platforms-instance-box") > -1 ||
         elementToReplace.attr("class").indexOf("home-platforms-instance-oval") > -1 || 
         elementToReplace.attr("class").indexOf("user-platforms-instance-box") > -1 ) {
        width = 80;
        height = 80;
-    } else if (elementToReplace.attr("class").indexOf("users-poster-face") > -1) {
-        width = 40;
-        height = 40;
+    } else {
+        width = img_url.indexOf("width=");
+        width = parseInt(img_url.substring(width + 6, width + 9).split('&')[0]);
+        height = img_url.indexOf('height=');
+        height = parseInt(img_url.substring(height + 7, height + 10).split('&')[0]);
     }
-    
-    elementToReplace.html('<img src="' + img_url + '" width="' + width + '" height="' + height + '"/>');
+    elementToReplace.append('<img src="' + img_url + '" width="' + width + '" height="' + height + '"/>');
 }
